46520448a86d9c62f1256755a86158a36767d5a3
Felipe Aguiar <felipeferreiraaguiar9@gmail.com>
2026-01-07 18:47:17 -0400

    .

--- packages/vulpes/src/services/API.ts
+++ packages/vulpes/src/services/API.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.API_URL ? "http://localhost:8080" : process.env.API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL
+    ? "http://localhost:8080"
+    : process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
